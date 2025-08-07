cc10a8fa5f7f04011b2cade11263f894c46a26af
RenanBarth <renan.barth@escola.pr.gov.br>
2025-08-07 08:32:07 -0300

    4444

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', botaoMostraPalavrasChave);
+
+function botaoMostraPalavrasChave () {
+    alert('fui clicado! ');
+}
